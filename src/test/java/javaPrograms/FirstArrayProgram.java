438662dc68a260b9782ba1e8f58219d15c7df054
sharmapawan123 <pawanshama483@gmail.com>
2025-01-29 15:18:02 +0530

    another commit

---
+++ src/test/java/javaPrograms/FirstArrayProgram.java
@@ -0,0 +1,18 @@
+package javaPrograms;
+
+public class FirstArrayProgram {
+
+	public static void main(String[] args) {
+		//Print default values
+		
+		String[] str = new String[4]; 
+		System.out.println(str[0]);
+		
+		int[] a = new int[1];
+		System.out.print(a[0]);
+		
+		
+
+	}
+
+}
