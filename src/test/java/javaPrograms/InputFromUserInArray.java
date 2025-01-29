292c8d0dd78adfee7853015c907b1a189ec69e54
sharmapawan123 <pawanshama483@gmail.com>
2025-01-29 15:59:56 +0530

    array prog

--- src/test/java/javaPrograms/InputFromUserInArray.java
+++ src/test/java/javaPrograms/InputFromUserInArray.java
@@ -1,5 +1,6 @@
 package javaPrograms;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputFromUserInArray {
@@ -17,6 +18,30 @@ public class InputFromUserInArray {
 		for (int i = 0; i < 5; i++) {
 			System.out.print(arr[i] + " ");
 		}
+		System.out.println();
+
+		// Print the even number only
+		System.out.println("Print the even number only");
+		for (int i = 0; i < 5; i++) {
+			if (arr[i] % 2 == 0) {
+				System.out.println(arr[i] + " is even ");
+			}
+
+			else {
+				if (arr[i] % 2 != 0) {
+					System.out.println(arr[i] + " is odd ");
+				}
+			}
+		}
+
+		// print the array using for-each loop
+		System.out.println("print the array using for-each loop");
+		for (int num : arr) {
+			System.out.println(num);
+		}
+
+		// Convert the int array in the String
+		System.out.print(Arrays.toString(arr));
 
 	}
 
