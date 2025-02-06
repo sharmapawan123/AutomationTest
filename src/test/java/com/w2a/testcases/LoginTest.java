bec3962a621866ff5998b1a5fd06f2550c0e53b8
sharmapawan123 <pawanshama483@gmail.com>
2025-02-06 11:51:22 +0530

    new scripts

---
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -0,0 +1,15 @@
+package com.w2a.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import com.w2a.base.TestBase;
+
+public class LoginTest extends TestBase {
+   
+	@Test
+	public void loginAsBankManager() throws InterruptedException
+	{
+		driver.findElement(By.xpath(or.getProperty("banlManager"))).click();
+		Thread.sleep(2000);
+	}
+}
